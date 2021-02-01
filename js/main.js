77e37a759a57859db4de5ec0ca2a8544ca3d159f
Shimu47 <73783607+Shimu47@users.noreply.github.com>
2021-02-01 12:12:40 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,181 @@
+ 
+ /* Web Design*/
+ var fm = new FluidMeter();
+    fm.init({
+      targetContainer: document.getElementById("fluid-meter-1"),
+      fillPercentage: 95,
+      options: {
+        fontFamily: "Raleway",
+        drawPercentageSign: true,
+        drawBubbles: true,
+        size: 300,
+        borderWidth: 19,
+        backgroundColor: "#e2e2e2",
+        foregroundColor: "#fafafa",
+        foregroundFluidLayer: {
+          fillStyle: "purple",
+          angularSpeed: 100,
+          maxAmplitude: 12,
+          frequency: 30,
+          horizontalSpeed: -150
+        },
+        backgroundFluidLayer: {
+          fillStyle: "pink",
+          angularSpeed: 100,
+          maxAmplitude: 9,
+          frequency: 30,
+          horizontalSpeed: 150
+        }
+      }
+    });
+
+/*Responsive Web Design*/
+var fm2 = new FluidMeter();
+    fm2.init({
+      targetContainer: document.getElementById("fluid-meter-2"),
+      fillPercentage: 95,
+options: {
+        fontFamily: "Raleway",
+        drawPercentageSign: true,
+        drawBubbles: true,
+        size: 300,
+        borderWidth: 19,
+        backgroundColor: "#e2e2e2",
+        foregroundColor: "#fafafa",
+        foregroundFluidLayer: {
+          fillStyle: "violet",
+          angularSpeed: 100,
+          maxAmplitude: 12,
+          frequency: 30,
+          horizontalSpeed: -150
+        },
+        backgroundFluidLayer: {
+          fillStyle: "pink",
+          angularSpeed: 100,
+          maxAmplitude: 9,
+          frequency: 30,
+          horizontalSpeed: 150
+        }
+      }
+    });
+
+/*PSD to HTML*/
+var fm3 = new FluidMeter();
+    fm3.init({
+      targetContainer: document.getElementById("fluid-meter-3"),
+      fillPercentage: 95,
+options: {
+        fontFamily: "Raleway",
+        drawPercentageSign: true,
+        drawBubbles: true,
+        size: 300,
+        borderWidth: 19,
+        backgroundColor: "#e2e2e2",
+        foregroundColor: "#fafafa",
+        foregroundFluidLayer: {
+          fillStyle: "blue",
+          angularSpeed: 100,
+          maxAmplitude: 12,
+          frequency: 30,
+          horizontalSpeed: -150
+        },
+        backgroundFluidLayer: {
+          fillStyle: "pink",
+          angularSpeed: 100,
+          maxAmplitude: 9,
+          frequency: 30,
+          horizontalSpeed: 150
+        }
+      }
+    });
+
+/*Wordpress Customization*/
+var fm4 = new FluidMeter();
+    fm4.init({
+      targetContainer: document.getElementById("fluid-meter-4"),
+      fillPercentage: 85,
+options: {
+        fontFamily: "Raleway",
+        drawPercentageSign: true,
+        drawBubbles: true,
+        size: 300,
+        borderWidth: 19,
+        backgroundColor: "#e2e2e2",
+        foregroundColor: "#fafafa",
+        foregroundFluidLayer: {
+          fillStyle: "#FD97BC",
+          angularSpeed: 100,
+          maxAmplitude: 12,
+          frequency: 30,
+          horizontalSpeed: -150
+        },
+        backgroundFluidLayer: {
+          fillStyle: "pink",
+          angularSpeed: 100,
+          maxAmplitude: 9,
+          frequency: 30,
+          horizontalSpeed: 150
+        }
+      }
+    });
+
+/*Website Content writing*/
+var fm5 = new FluidMeter();
+    fm5.init({
+      targetContainer: document.getElementById("fluid-meter-5"),
+      fillPercentage: 90,
+options: {
+        fontFamily: "Raleway",
+        drawPercentageSign: true,
+        drawBubbles: true,
+        size: 300,
+        borderWidth: 19,
+        backgroundColor: "#e2e2e2",
+        foregroundColor: "#fafafa",
+        foregroundFluidLayer: {
+          fillStyle: "#00A1D5",
+          angularSpeed: 100,
+          maxAmplitude: 12,
+          frequency: 30,
+          horizontalSpeed: -150
+        },
+        backgroundFluidLayer: {
+          fillStyle: "pink",
+          angularSpeed: 100,
+          maxAmplitude: 9,
+          frequency: 30,
+          horizontalSpeed: 150
+        }
+      }
+    });
+
+/*On Page SEO*/
+var fm6 = new FluidMeter();
+    fm6.init({
+      targetContainer: document.getElementById("fluid-meter-6"),
+      fillPercentage: 90,
+options: {
+        fontFamily: "Raleway",
+        drawPercentageSign: true,
+        drawBubbles: true,
+        size: 300,
+        borderWidth: 19,
+        backgroundColor: "#e2e2e2",
+        foregroundColor: "#fafafa",
+        foregroundFluidLayer: {
+          fillStyle: "#F634A3",
+          angularSpeed: 100,
+          maxAmplitude: 12,
+          frequency: 30,
+          horizontalSpeed: -150
+        },
+        backgroundFluidLayer: {
+          fillStyle: "pink",
+          angularSpeed: 100,
+          maxAmplitude: 9,
+          frequency: 30,
+          horizontalSpeed: 150
+        }
+      }
+    });
+
